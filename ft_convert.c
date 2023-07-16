e661ebb64942bc82b03d1b2853477aad2efff518
markus <nummimarkus@gmail.com>
2023-07-16 13:00:01 +0300

    norminette fixes

--- ft_convert.c
+++ ft_convert.c
@@ -6,14 +6,11 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 00:45:19 by mnummi            #+#    #+#             */
-/*   Updated: 2023/07/12 00:45:24 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/16 12:59:51 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "ft_printf.h"
-//converting to hex
-char *ft_convert(unsigned long int num, int base, int lowercase)
-{
+//converting decimal to hex
 /*
 To convert int to hex:
 Divide the integer by 16
@@ -21,17 +18,21 @@ Divide the result of the division by 16
 Take the remainder of the division and convert it to a hexadecimal digit
 Repeat steps one and two until the result of the division is 0
 */
+//ptr = &buffer ->  assigning ptr to end of buffer
+
+char	*ft_convert(unsigned long int num, int base, int lowercase)
+{
 	char	*ptr;
-	char	buffer[50]; //assuming 50 chars is long enough
+	char	buffer[50];
 	char	representation[16];
-	int	res;
+	int		res;
 
 	if (lowercase == 1)
 		ft_strcpy(representation, "0123456789abcdef");
 	else
 		ft_strcpy(representation, "0123456789ABCDEF");
-	ptr = &buffer[49]; // assigning ptr to end of buffer
-	*ptr = '\0'; //setting null terminating char to end 
+	ptr = &buffer[49];
+	*ptr = '\0';
 	if (num <= 0)
 	{
 		*--ptr = '0';
