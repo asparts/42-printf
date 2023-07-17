262e187b29ade7ff750dff5d89d94f6cc6dfe6ba
markus <nummimarkus@gmail.com>
2023-07-18 01:24:53 +0300

    norminette fixes

--- ft_print_int.c
+++ ft_print_int.c
@@ -6,30 +6,31 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 01:48:13 by mnummi            #+#    #+#             */
-/*   Updated: 2023/07/12 01:48:24 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/18 01:24:39 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 // to print %i & %d
-int    ft_print_int(int n)
+//nbr ->  to handle int_MIN / -2147483648
+int	ft_print_int(int n)
 {
-    int len;
-    long int nbr; // to handle int_MIN / -2147483648
+	int			len;
+	long int	nbr;
 
-    len = 0;
-    nbr = n;
-    if (nbr < 0)
-    {
-        len += ft_putchar('-');
-        nbr = nbr * -1;
-    }
-    if (nbr < 10)
-        len += ft_putchar(nbr + '0');
-    else
-    {
-        len += ft_print_int(nbr / 10);
-        len += ft_putchar(nbr % 10 + '0');
-    }
-    return (len);
+	len = 0;
+	nbr = n;
+	if (nbr < 0)
+	{
+		len += ft_putchar('-');
+		nbr = nbr * -1;
+	}
+	if (nbr < 10)
+		len += ft_putchar(nbr + '0');
+	else
+	{
+		len += ft_print_int(nbr / 10);
+		len += ft_putchar(nbr % 10 + '0');
+	}
+	return (len);
 }
