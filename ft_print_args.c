bd5539d9babcd565828099f2bfae6ff9d12051e5
markus <nummimarkus@gmail.com>
2023-07-17 22:54:28 +0300

    changed switch-case to if-else since it's forbidden by norminette

--- ft_print_args.c
+++ ft_print_args.c
@@ -17,36 +17,25 @@ int	ft_print_args(char c, va_list args)
 	unsigned int	i;
 
     i = 1;
-	switch (c)
-	{
-		case 'c' :
-			return(ft_putchar(va_arg(args, int)));
-			break;
-		case 's' :
-			return(ft_putstr(va_arg(args, char*)));
-			break ;
-         case 'p' :
-            return(ft_print_ptr(va_arg(args, void*)));
-			break ;
-        case 'd' :
-            return (ft_print_int(va_arg(args, int)));
-			break ;
-        case 'i' :
-            return (ft_print_int(va_arg(args, int)));
-			break ;
-        case 'u' :
-            return (ft_print_unsigned(va_arg(args, unsigned int)));
-			break ;
-		case 'x' : 
-			return(ft_print_hex(va_arg(args, unsigned int), 1));
-			break ;
-        case 'X' : 
-			return(ft_print_hex(va_arg(args, unsigned int), 0));
-			break ;
-		case '%' : 
-			return (ft_putchar('%'));
-			break ;
-		default:
-			return (-1);
-	}
+
+	if (c == 'c')
+		return(ft_putchar(va_arg(args, int)));
+	else if (c == 's')
+		return(ft_putstr(va_arg(args, char*)));
+	else if (c == 'p')
+		return(ft_print_ptr(va_arg(args, void*)));
+	else if (c == 'd')
+		return (ft_print_int(va_arg(args, int)));
+	else if (c == 'i')
+		return (ft_print_int(va_arg(args, int)));
+	else if (c == 'u')
+		return (ft_print_unsigned(va_arg(args, unsigned int)));
+	else if (c == 'x')
+		return(ft_print_hex(va_arg(args, unsigned int), 1));
+	else if (c == 'X')
+		return(ft_print_hex(va_arg(args, unsigned int), 0));
+	else if (c == '%')
+		return (ft_putchar('%'));
+	else
+		return (-1);
 }
